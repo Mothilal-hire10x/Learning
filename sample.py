1cf6f7a70fa0811d6e4f2e8723ed50e8de99bf86
Mothilal-hire10x <mothilal@hire10x.ai>
2024-11-27 07:48:48 +0000

    added changes

--- sample.py
+++ sample.py
@@ -18,5 +18,8 @@ def calculate_sum(request: SumRequest):
     result = request.num1 + request.num2
     return {"num1": request.num1, "num2": request.num2, "sum": result}
 
+
+print("Hello World..!")
+
 # To run this API, save it as a Python file (e.g., `main.py`), then run:
 # uvicorn main:app --reload
